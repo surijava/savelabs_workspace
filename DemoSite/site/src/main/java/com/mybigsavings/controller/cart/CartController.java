d908222854df36ad897ac861725e2db283fb393b
surijava <surendracho@gmail.com>
2015-12-18 13:35:26 +0530

    test

--- DemoSite/site/src/main/java/com/mybigsavings/controller/cart/CartController.java
+++ DemoSite/site/src/main/java/com/mybigsavings/controller/cart/CartController.java
@@ -59,7 +59,6 @@ public class CartController extends BroadleafCartController {
             returnPath += " :: ajax";
         }
         System.out.println("india hai");
-        System.out.println("india hai");
         System.out.println("suri");
         System.out.println("venky ");
         return returnPath;
